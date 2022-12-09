7a9bba741b651aa886a4f1d3036698fb16c26bd9
Nomoya Magazi <nomoya.magazi@gmail.com>
2022-12-09 03:31:09 -0800

    delete

--- 0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-#creates a file in /tmp
-
-file {'tmp/school':
-ensure => present,
-content => 'I love puppet',
-mode => '0744',
-owner => 'www-data',
-group => 'www-data',
-}
--- 1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# install flask version 2.1.0
-
-package { 'flask':
-ensure => 'installed 2.1.0',
-provider => 'pip3',
-}
