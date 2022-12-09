7ec285cd2b0e9d91247dc95c5c36af30f84e086a
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-12-09 14:03:41 +0200

    Update 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # a manifests that terminates a process called killmenow
 
 exec { 'pkill':
-ensure => present,
-command => 'pkill killmenow',
+  ensure  => present,
+  command => 'pkill killmenow',
 }
