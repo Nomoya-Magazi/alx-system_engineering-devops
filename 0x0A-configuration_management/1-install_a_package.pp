6e64d53757c8b648b56a998b0b1ac66d13cbdf30
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2022-12-09 13:56:49 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# install flask version 2.1.0
+# Install flask (2.1.0)
 package {'flask':
-  ensure   => ' installed 2.1.0',
-  provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
